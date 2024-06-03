9be3c191c6ca48d32457f53973222b0771851584
saksham.jaiswal <saksham.jaiswal@unthinkable.co>
2024-06-04 00:19:57 +0530

    halt marker iOS

---
+++ ios/Plugin/Views/AlertBusMarkerHalt.swift
@@ -0,0 +1,27 @@
+//
+//  AlertBusMarkerHalt.swift
+//  App
+//
+//  Created by Ankit Saini on 25/06/23.
+//
+
+import UIKit
+
+class AlertBusMarkerHalt: UIView {
+    
+    @IBOutlet weak var BusNumberText: UILabel!
+    @IBOutlet weak var AlertSnippet: UILabel!
+    @IBOutlet weak var IgnitionImage: UIImageView!
+    @IBOutlet weak var AlertHaltCard: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        AlertHaltCard.layer.cornerRadius = 15
+        AlertHaltCard.layer.masksToBounds = false
+    }
+    
+
+    class func instanceFromNib() -> AlertBusMarkerHalt {
+        return UINib(nibName: "AlertBusMarkerHalt", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! AlertBusMarkerHalt
+    }
+}
