1ba6e27fdfdb7650bd7d3ad9aaf2f757b557c2d3
Abhay Agrahary <abhay.agrahary@unthinkable.co>
2025-11-08 13:24:26 +0530

    Stop arrival indo window

--- ios/Plugin/Views/StopArrivalInfoWindow.swift
+++ ios/Plugin/Views/StopArrivalInfoWindow.swift
@@ -1,31 +1,33 @@
 //
 //  StopArrivalInfoWindow.swift
-//  Pods
+//  Plugin
 //
-//  Created by Abhay Agrahary on 06/11/25.
+//  Created by Abhay Agrahary on 08/11/25.
+//  Copyright © 2025 Max Lynch. All rights reserved.
 //
-
 
 import UIKit
 
 class StopArrivalInfoWindow: UIView {
     
-    @IBOutlet weak var busTitle: UILabel!
+    @IBOutlet weak var arrivalView: UIView!
+    @IBOutlet weak var BusTitle: UILabel!
     @IBOutlet weak var arrivalTime: UILabel!
     @IBOutlet weak var departureTime: UILabel!
-    @IBOutlet weak var StopArrivalCardView: UIView!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        // Applying card-like background to StopArrivalCardView
-        StopArrivalCardView.layer.cornerRadius = 4
-        StopArrivalCardView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.08).cgColor
-        StopArrivalCardView.layer.shadowOpacity = 1
-        StopArrivalCardView.layer.shadowOffset = CGSize(width: 0, height: 0.62)
-        StopArrivalCardView.layer.shadowRadius = 1.86
-        StopArrivalCardView.layer.masksToBounds = false
-        StopArrivalCardView
+        // Applying card-like background to arrivalView
+        arrivalView.layer.cornerRadius = 4
+        arrivalView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.08).cgColor
+        arrivalView.layer.shadowOpacity = 1
+        arrivalView.layer.shadowOffset = CGSize(width: 0, height: 0.62)
+        arrivalView.layer.shadowRadius = 1.86
+        arrivalView.layer.masksToBounds = false
+        arrivalView.backgroundColor = UIColor.white
+
     }
 
     class func instanceFromNib() -> StopArrivalInfoWindow {
