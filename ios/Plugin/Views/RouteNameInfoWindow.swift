aece3d87f84ddaab60ca2a196dc10284180de2ee
Ankit Singhania <ankit.s@chalo.com>
2026-03-14 13:18:55 +0530

    Updated info window styling

--- ios/Plugin/Views/RouteNameInfoWindow.swift
+++ ios/Plugin/Views/RouteNameInfoWindow.swift
@@ -1,25 +1,13 @@
-//
-//  ViewController.swift
-//  Plugin
-//
-//  Created by macbook on 13/03/26.
-//  Copyright © 2026 Max Lynch. All rights reserved.
-//
-
 import UIKit
 
 class RouteNameInfoWindow: UIView {
-
     @IBOutlet weak var routeNameCardView: UIView!
-    
     @IBOutlet weak var infoTitle: UILabel!
-    
 
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        // Applying card-like background to lastUpdatedCardView
+        // Configure the card view
         routeNameCardView.layer.cornerRadius = 4
         routeNameCardView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.08).cgColor
         routeNameCardView.layer.shadowOpacity = 1
@@ -27,11 +15,20 @@ class RouteNameInfoWindow: UIView {
         routeNameCardView.layer.shadowRadius = 1.86
         routeNameCardView.layer.masksToBounds = false
         routeNameCardView.backgroundColor = UIColor.white
-        
+
+        // Configure the label
+        infoTitle.numberOfLines = 1 // Ensure single-line text
+        infoTitle.lineBreakMode = .byClipping // Prevent wrapping or truncation
+        infoTitle.setContentCompressionResistancePriority(.defaultLow, for: .horizontal) // Allow expansion
     }
-    
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // Ensure the view adjusts its size dynamically
+        self.layoutIfNeeded()
+    }
+
     class func instanceFromNib() -> RouteNameInfoWindow {
         return UINib(nibName: "RouteNameInfoWindow", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! RouteNameInfoWindow
     }
-    
 }
